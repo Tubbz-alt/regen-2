7b6a233a9a1b704a6ca1224d2e6ca7394f6f2dbc
Michiru Morishita <kokser.merio@gmail.com>
2020-02-06 15:18:30 +0800

    add license file

--- regen.py
+++ regen.py
@@ -1,101 +1,155 @@
 from mako.template import Template
+from typing import List
+from typing import Union
 
 
 class Field:
-    # Unique identify
-    name = ""
-    bit_width = 1
-    bit_offset = 0
+    """
+    Field class model the **Field** in **Register**. Field is the atom object in Register Map generation, usually for
+    one configuration/statistics parameter. One field can span multi-bits or at least one bit in Register. One
+    Register can contain one or more fields.
+    """
 
-    # Mandatory attributes
-    reset = 0
-    access_type = 'RW'
-    _parent = None
+    def __init__(self, name: str, bit_offset=0, bit_width=1, access_type='RW', reset=0, description: str = None,
+                 _parent: 'Register' = None) -> None:
+        # Unique identify
+        self._name = name
+        self._bit_offset = bit_offset
+        self._bit_width = bit_width
 
-    # Optional attributes
-    description = ""
-
-    def __init__(self, name, access_type='RW', bit_offset=0, bit_width=1, reset=0, parent=None):
-        self.name = name
+        # Mandatory attributes
         self.access_type = access_type
-        self.bit_width = bit_width
-        self.bit_offset = bit_offset
         self.reset = reset
-        self._parent = parent
+
+        # Optional attributes
+        self.description = description
+
+        # Data storage
+        self._parent = _parent
+
+    def __cmp__(self, other) -> int:
+        if self.bit_offset < other.bit_offset:
+            return -1
+        elif self.bit_offset > other.bit_offset:
+            return 1
+        else:
+            return 0
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def bit_offset(self) -> int:
+        return self._bit_offset
+
+    @property
+    def bit_width(self) -> int:
+        return self._bit_width
+
+    @property
+    def left_bit(self) -> int:
+        return self.bit_offset + self.bit_width - 1
+
+    @property
+    def right_bit(self) -> int:
+        return self.bit_offset
+
+    @property
+    def self_range(self) -> str:
+        """
+        Get the string of range in "[BIT_WIDTH-1:0]" format
+        :param: None
+        :return: The range in string
+        """
+        if self.bit_width == 1:
+            return ''
+        else:
+            return f'[{self.bit_width - 1}:0]'
+
+    @property
+    def full_range(self) -> str:
+        """
+        Get the string of range in [BIT_WIDTH+BIT_OFFSET-1:BIT_OFFSET] format
+        :param: None
+        :return: The full range in string
+        """
+        if self.bit_width == 1:
+            return f'[{self.bit_offset + self.bit_width - 1}]'
+        else:
+            return f'[{self.bit_offset + self.bit_width - 1}:{self.bit_offset}]'
 
     @staticmethod
-    def from_dict(d: dict):
-        ret = Field(name=d['name'])
+    def from_dict(d: dict) -> "Field":
+        """
+        Build a Field object from a dict object (for easy parameter passing). Remember
+        :param d: A dict object stores attribute information.
+        :return: A Field object
+        """
+        if d.get('name'):
+            ret = Field(name=d['name'])
+        else:
+            raise ValueError("'name' must be specified in dict's keys")
 
         # Override default values
-        if d.get('access_type'):
-            ret.access_type = d['access_type']
-        if d.get('bit_offset'):
-            ret.bit_offset = d['bit_offset']
+        for a in ['bit_offset', 'bit_width', 'access_type', 'reset', 'description']:
+            if d.get(a):
+                setattr(ret, a, d[a])
 
         return ret
 
-    def full_name(self, delimiter='.'):
+    def full_name(self, delimiter='.') -> str:
         """
-        Get the string of field name in REGISTER.FIELD format
+        Get the string of field name in "REGISTER.FIELD" format
+        :param delimiter: The delimiter character that separates Register name and Filed name
+        :return: The full name of field in string
         """
         if self._parent:
             return self._parent.name + delimiter + self.name
         else:
             return self.name
 
-    def self_range(self):
-        """
-        Get the string of range in [BIT_WIDTH-1:0] format
-        """
-        if self.bit_width == 1:
-            return ""
-        else:
-            return "[" + str(self.bit_width - 1) + ":" + "0]"
-
-    def full_range(self):
-        """
-        Get the string of range in [BIT_WIDTH+BIT_OFFSET-1:BIT_OFFSET] format
-        """
-        if self.bit_width == 1:
-            return "[" + str(self.bit_width + self.bit_offset - 1) + "]"
-        else:
-            return "[" + str(self.bit_width + self.bit_offset - 1) + ":" + str(self.bit_offset) + "]"
-
 
 class Register:
-    # Unique identifier
-    name = None
-    address_offset = None
+    """
+    Register class models **Registers** in **Register Map**. Register holds one or more fields. There can be one or
+    more Registers in one Register Map. Registers in Register Map should have unique name and address offset. Fields
+    in one Registers should not overlap with each other. Fields in one Register should have unique name. Bits not
+    occupied by any fields are considered to be "reserved" (not writeable, reading returns zero).
+    """
 
-    # Mandatory attributes
-    _parent = None
+    def __init__(self, name: str, address_offset=0, description: str = None, _parent: 'RegisterMap' = None):
+        # Unique identifier
+        self._name = name
+        self._address_offset = address_offset
 
-    # Optional attributes
-    description = None
+        # Mandatory attributes
 
-    # Data storage
-    _fields = None
-
-    def __init__(self, name, address_offset=0, description='', parent=None):
-        self.name = name
-        self.address_offset = address_offset
-        self._parent = parent
+        # Optional attributes
         self.description = description
+
+        # Data storage
         self._fields = list()
+        self._parent = _parent
 
     def __len__(self):
         return self._fields.__len__()
 
     def __iter__(self):
-        return iter(self._fields)
+        return iter(self.fields)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> Field:
         """
-        Get the field in Register using the API:
-        REG['FIELD_NAME']
+        Get the field in Register using field's `name`.
+
+        :param: Field's name.
+        :return The required Field object.
         """
-        return self.get_field(item)
+
+        if self.has_field(item):
+            return self.get_field(item)
+        else:
+            raise KeyError
 
     def __setitem__(self, key, value):
         """
@@ -109,7 +163,7 @@ class Register:
             bit_width = 1
         elif type(key) is slice:
             if key.step:
-                print("Reigster slice with none 1 step is not supported!\n")
+                print("Register slice with none 1 step is not supported!\n")
                 raise KeyError
             bit_offset = key.stop
             bit_width = key.start - key.stop + 1
@@ -117,10 +171,31 @@ class Register:
             raise KeyError
 
         # Create a new Field
-        field = Field(name=value, bit_offset=bit_offset, bit_width=bit_width, parent=self)
+        field = Field(name=value, bit_offset=bit_offset, bit_width=bit_width)
 
         # Add the filed to Register
-        self.add_field(field)
+        if not self.add_field(field):
+            raise ValueError
+
+    def __cmp__(self, other):
+        if self.address_offset < other.address_offset:
+            return -1
+        elif self.address_offset > other.address_offset:
+            return 1
+        else:
+            return 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def address_offset(self):
+        return self._address_offset
+
+    @property
+    def fields(self):
+        return sorted(self._fields, key=lambda x: x.bit_offset, reverse=True)
 
     @staticmethod
     def from_tuple(t):
@@ -128,113 +203,181 @@ class Register:
         Build a Register from a tuple. The tuple should be in format:
         (name, description)
         """
-        ret = Register(name=t(0), description=t(1))
+        if len(t) > 0:
+            ret = Register(name=t(0), description=t(1))
+        else:
+            raise ValueError
+
+        for i, a in enumerate(['address_offset', 'description']):
+            if len(t) > i + 1:
+                setattr(ret, a, t[i])
+
         return ret
 
     @staticmethod
     def from_dict(d):
         """
-        Build a Register Object from a dict. The dict should be in format:
-        { 'name': name_of_register, 'description'=description_of_register }
+        Build a Register Object from a dict.
+        :param d: A dict object holds the information in attributes
+        :return: A Register object
         """
-        ret = Register(name=d['name'], description=d['description'])
+        if d.get('name'):
+            ret = Register(name=d['name'])
+        else:
+            raise ValueError
+
+        for a in ['address_offset', 'description']:
+            if d.get(a):
+                setattr(ret, a, d[a])
+
         return ret
 
     def add_field(self, field: Field):
         """
         Add a Field object to this Register
+        :param field: The Field object to be add
+        :return: If add operation is success
         """
         # Check if field name is occupy
         if self.has_field(field.name):
-            raise ValueError
+            return False
 
-        # Check if overlap existing fields
+        # Check if overlap with existing fields
         overlap = 0
-        overlaped_field = None
+        overlapped_field = None
         for f in self._fields:
             if not (field.bit_offset > (f.bit_offset + f.bit_width - 1) or
                     f.bit_offset > (field.bit_offset + field.bit_width - 1)):
                 overlap = 1
-                overlaped_field = f
+                overlapped_field = f
                 break
         if overlap:
-            print("Overlap with field {0}".format(overlaped_field.full_name()))
-            raise ValueError
+            print("Overlap with field {0}".format(overlapped_field.full_name()))
+            return False
 
         # Check pass, add this field
+        field._parent = self
         self._fields.append(field)
+        return True
 
     def has_field(self, name: str):
         """
         Test if given name of the field is exists in register
+        :param name: The name of the field
+        :return: If the field with given name exists
         """
         if type(name) is str:
-            return name in {f.name for f in self._fields}
+            return name in [f.name for f in self._fields]
         else:
             raise TypeError
 
-    def get_field(self, name: str):
+    def get_field(self, name: str) -> Union[Field, None]:
         """
         Get the field in Register by name
+        :param name: The name of the field
+        :return: The Filed with the given name, or None if not exists
         """
         if type(name) is str:
-            idx = [f.name for f in self._fields].index(name)
-            return self._fields[idx]
+            nl = [f.name for f in self._fields]
+            if name in nl:
+                idx = nl.index(name)
+                return self._fields[idx]
+            else:
+                return None
         else:
             raise TypeError
+
+    def remove_field(self, name: str) -> None:
+        f = self.get_field(name)
+        if f:
+            self._fields.remove(f)
+
+    def clear_fields(self) -> None:
+        """
+        Clear all fields without touching other parameters
+        :return: None
+        """
+        self._fields.clear()
+
+    def all_fields(self, sort=True, reverse=True) -> List[Field]:
+        """
+        Get all fields in a list, sorted by bit_offset
+        :param sort: If true, sort the fields by bit offset
+        :param reverse: If true, when sort, reverse the order
+        :return: A list with all fields
+        """
+        if sort:
+            return sorted(self._fields, key=lambda x: x.bit_offset, reverse=reverse)
+        else:
+            return self._fields
 
 
 class RegisterMap:
-    # Unique identifier
-    name = ""
+    """
+    **RegisterMap** models the Register Map. A Register Map is a "block" which will generate a Verilog module. One
+    Register Map contains one or more Registers. Registers in one Map should have unique name and address offset.
+    """
 
-    # Required attributes
-    base_address = 0
-    data_width = 32
-    address_width = 0 # 0 = Auto address width (minimal)
+    def __init__(self, name: str, base_address=0, data_width=32, address_width=0, description: str = None) -> None:
+        # Unique identifier
+        self._name = name
 
-    # Optional attributes
-    description = ""
-
-    # Data storage
-    _registers = []
-
-    def __init__(self, name, base_address=0, data_width=32, address_width=0):
-        self.name = name
+        # Required attributes
         self.base_address = base_address
         self.data_width = data_width
         self.address_width = address_width
 
-    def __iter__(self):
-        return iter(self._registers)
+        # Optional attributes
+        self.description = description
 
-    def __setitem__(self, key, value):
+        # Data storage
+        self._registers = list()
+
+    def __iter__(self):
+        return iter(self.registers)
+
+    def __setitem__(self, key: int, value: str) -> None:
         """
         Provides a simplified API to instance Register in RegisterMap
         The format should like:
         REG_MAP[0] = 'CTRL'
-        """
-        if type(key) is not int:
-            raise KeyError
 
-        if type(value) is not str:
-            raise ValueError
+        :param key: The address offset of new Register
+        :param value: The name of new Register
+        :return: None
+        """
 
         # Create a new Register
-        reg = Register(name=value, address_offset=key, parent=self)
+        reg = Register(name=value, address_offset=key)
+
         # Add it to the RegisterMap
         self.add_register(reg)
 
     def __getitem__(self, name_or_addr):
-        return self.get_register(name_or_addr)
+        if self.has_register(name_or_addr):
+            return self.get_register(name_or_addr)
+        else:
+            raise KeyError(name_or_addr)
 
     def __len__(self):
         return self._registers.__len__()
 
-    def has_register(self, name_or_addr):
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def registers(self):
+        return sorted(self._registers, key=lambda x: x.address_offset)
+
+    def has_register(self, name_or_addr: Union[str, int]) -> bool:
         """
-        Test if the given register exists in this RegisterMap, using the unique attribute `name` or `address_offset`
+        Test if the given register exists in this RegisterMap, using the unique attribute `name` or `address_offset`.
+
+        :param name_or_addr: The name or the address offset of the register
+        :return: True if the map has the desired register, else False
         """
+
         # Check if name exists
         if type(name_or_addr) is str:
             return name_or_addr in {r.name for r in self._registers}
@@ -244,7 +387,7 @@ class RegisterMap:
         else:
             raise TypeError
 
-    def get_register(self, name_or_addr):
+    def get_register(self, name_or_addr) -> Register:
         """
         Get the register with given name or address offset in map
         :param name_or_addr:
@@ -263,34 +406,66 @@ class RegisterMap:
             # No register with name or address offset
             raise ValueError
 
-    def remove_register(self, name_or_addr):
+    def remove_register(self, name_or_addr) -> None:
         """
         Remove register in current map
+
+        :return: None
         """
         t = self.get_register(name_or_addr)
-        self._registers.remove(t)
+        if t:
+            self._registers.remove(t)
+        else:
+            raise ValueError(self.remove_register.__name__)
 
-    def clear_registers(self):
+    def clear_registers(self) -> None:
         """
         Remove all registers in current map, leave name and other attributes untouched
+
+        :return: None
         """
+
         self._registers.clear()
 
-    def add_register(self, reg: Register):
-        if self.has_register(reg.name) or self.has_register(reg.address_offset):
-            # Duplicate register
-            raise ValueError
+    def add_register(self, reg: Register) -> None:
+        """
+        Add a Register object to internal register list. If duplicate identity (`name`, `address_offset`) found,
+        an error is raise.
+
+        :param reg: The Register object to be added
+        :return: None
+        """
+
+        # Duplicate check
+        if self.has_register(reg.name):
+            raise ValueError('duplicate name of register')
+
+        if self.has_register(reg.address_offset):
+            raise ValueError('duplicate address offset of register')
+
         # Append the new register to register list
+        reg._parent = self
         self._registers.append(reg)
 
-    def drc(self):
+    def drc(self) -> bool:
         """
         DRC (Design Rule Check) function
-        """
-        # TODO
-        print("Function \"drc\" not implemented!")
 
-    def gen_verilog(self, output_file=None, template_file="template/bram.v"):
+        :return: If the DRC check is pass
+        """
+
+        # TODO
+        raise SystemError("function \"drc\" not implemented!")
+
+    def gen_verilog(self, output_file: str = None, template_file="template/bram.v") -> None:
+        """
+        Generate the Verilog file from the Register Map.
+
+        :param output_file: Path of output file.
+        :param template_file: The template file to use when generating the Verilog file
+        :return: None
+        """
+
         # Render template
         t = Template(filename=template_file)
         s = t.render(regMap=self)
